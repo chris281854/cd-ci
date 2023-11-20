ca65b3bdd9e29bdc2aa8b3ad23429a5e742f3e9b
chris281854 <christopherajimenez88@gmail.com>
2023-11-19 20:27:28 -0400

    first action created on my own repository

---
+++ .github/actions/hello/index.js
@@ -0,0 +1,22 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+try {
+core.debug('Debug message');
+core.warning('Warning message');
+core.error('Error message');
+
+
+const name = core.getInput('who_to_greet');
+
+consolog.log(`Hello ${name}!`);
+const time = new Date();
+core.setOutput("time", time.toString);
+core.exportVariable("Hello_Time", time.toString);
+core.startGroup("Logging context");
+console.log(JSON.stringify(github.context,null,2));
+core.endGroup();
+
+}catch(error){
+    core.setFailed(error.message);
+}
+
